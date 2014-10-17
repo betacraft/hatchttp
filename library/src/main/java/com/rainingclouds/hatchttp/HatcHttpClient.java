2085c862481fced52a94297fff397ffdc46e891d
Akshay Deo <akshay@rainingclouds.com>
2014-10-17 19:55:28 +0530

    - added efficient thread pool executor

--- library/src/main/java/com/rainingclouds/hatchttp/HatcHttpClient.java
+++ library/src/main/java/com/rainingclouds/hatchttp/HatcHttpClient.java
@@ -2,8 +2,9 @@ package com.rainingclouds.hatchttp;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
+import com.rainingclouds.hatchttp.executors.EfficientThreadPoolExecutor;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * NingHttp Client
@@ -14,7 +15,7 @@ class HatcHttpClient {
     final static AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder()
             .setAllowPoolingConnection(true)
             .setFollowRedirects(true)
-            .setExecutorService(Executors.newFixedThreadPool(2))
+            .setExecutorService(EfficientThreadPoolExecutor.get(3,10,1, TimeUnit.MINUTES,4,"hatcHttp_workers"))
             .setRequestTimeoutInMs(30000)
             .build();
     private static AsyncHttpClient asyncHttpClient = new AsyncHttpClient(config);
